9ae3b960fa7056b1f07fd0b8e4edf5176a45f7eb
sergio-git22 <sergio.rmx22@gmail.com>
2025-03-19 10:51:04 +0100

    añadido registro

---
+++ JS/auth.js
@@ -0,0 +1,45 @@
+import { APIKEY, BASE_URL } from "./config.js";
+
+const name = document.getElementById("name");
+const userName = document.getElementById("userName");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+const alertPass = document.getElementById("passwordNot");
+
+// REGISTRARSE
+const btnRegistro = document.getElementById("btnRegistro");
+if (btnRegistro) {
+  btnRegistro.addEventListener("click", register);
+  if (password.value !== password2.value) {
+    alertPass.classList.remove("hidden");
+  }
+}
+
+async function register() {
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      apikey: APIKEY,
+      "Content-Type": "application.json",
+    },
+    body: JSON.stringify({
+      name: name.value,
+      userName: userName.value,
+      email: email.value,
+      password: password.value,
+    }),
+  };
+
+  const response = await fetch(`${BASE_URL}/auth/v1/signup`, requestOptions);
+  if (!response.ok) {
+    alert("Error en el registro");
+  }
+
+  const result = await response.json();
+
+  localStorage.setItem("token", result.access_token);
+  localStorage.setItem("user_id", result.user.id);
+
+  window.location.href = "/dashboard.html";
+}
---
+++ JS/config.js
@@ -0,0 +1,5 @@
+const APIKEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZqZnN2aXJwdnhnd3lnZnJubXZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIyOTI5NjgsImV4cCI6MjA1Nzg2ODk2OH0.jTLEb5LPOTpnkU9FPNUUncKHBiMUrBwV11U8UNejaQg";
+const BASE_URL = "https://vjfsvirpvxgwygfrnmvf.supabase.co";
+
+export { APIKEY, BASE_URL };
