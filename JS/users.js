e1d0a84e28ad2d683f94ee76be81adf457310349
sergio-git22 <sergio.rmx22@gmail.com>
2025-04-05 13:46:56 +0200

    GET & PRINT users

--- JS/users.js
+++ JS/users.js
@@ -1,7 +1,8 @@
 import { APIKEY, BASE_URL } from "config.js";
 import { logout, getToken } from "auth.js";
 
-const datos = document.getElementById("perfil");
+const perfil = document.getElementById("perfil");
+const currentUserId = localStorage.getItem("user_id");
 
 // OBTENER USUARIOS
 async function getUsers() {
@@ -23,6 +24,49 @@ async function getUsers() {
   const result = await response.json();
 
   console.log(result);
+  printUser(result);
+}
+
+// OBTENER INFO DEL USUARIO
+async function getInfoUser(currentUserId) {
+  try {
+    const response = await fetch(
+      `${BASE_URL}/rest/v1/Users?user_id=eq.${user_id}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          apikey: APIKEY,
+          Authorization: `Bearer ${APIKEY}`,
+        },
+      }
+    );
+
+    const data = await response.json();
+    return data.length > 0 ? data[0] : null;
+  } catch (error) {
+    console.error("Error al obtener datos del usuario:", error);
+    return null;
+  }
+}
+
+// PINTAR DATOS DE USUARIO
+async function printUser() {
+  if (!currentUserId) {
+    console.error("No hay usuario logueado");
+    return;
+  }
+
+  const userInfo = await getInfoUser(currentUserId);
+
+  if (!userInfo) {
+    console.error("No se pudo obtener la información del usuario");
+    return;
+  }
+
+  perfil.innerHTML = `
+  <h1>Mi perfil</h1>
+  <p>Nombre y apellidos:${user_name} || "Desconocido"</p>`;
 }
 
 getUsers();
